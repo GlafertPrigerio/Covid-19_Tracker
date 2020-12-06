872611f99b42ee63dc364b7187d9949db4b0fda7
GlafertPrigerio <69837693+GlafertPrigerio@users.noreply.github.com>
2020-12-07 01:41:54 +0800

    Project Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Global from './pages/Global'
+import Indonesia from './pages/Indonesia'
+import Provinsi from './pages/Provinsi'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+    return (
+        <header className="warna2">
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/Global">
+                                Global
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Indonesia">
+                                Indonesia
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Provinsi">
+                                Provinsi
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Switch>
+                <Route path="/Indonesia">
+                    <Indonesia />
+                </Route>
+                <Route path="/provinsi">
+                    <Provinsi />
+                </Route>
+                <Route path="/Global">
+                    <Global />
+                </Route>
+            </Switch>
+        </Router>
+        </header>
+    )
 }
 
 export default App;
